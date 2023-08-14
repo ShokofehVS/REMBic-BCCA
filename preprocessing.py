002714475ec3c82b150df014e440060fd4bfbf6d
NK <n_k>
2023-08-14 15:44:42 +0200

    data preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,142 @@
+import numpy as np  # Numerical computing library
+import pandas as pd  # Data manipulation library
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder  # Preprocessing transformers
+
+
+def categorical_data_to_numerical_encoding(df_cat):
+    # 5--> Encoding categorical variables
+
+    # Creating an instance of the OneHotEncoder with sparse=False to obtain a dense array
+    encoder = OneHotEncoder(sparse=False)
+    encoded_data_cat = encoder.fit_transform(df_cat)
+
+    # Printing the head (first few rows) of the df_cat DataFrame
+    print(df_cat.head())
+
+    # Printing the encoded representation of the first row of the categorical data
+    print(encoded_data_cat[0])
+
+    # Specifying the categorical columns to be encoded
+    categorical_columns = df_cat.columns[[0, 1, 2, 3, 4]]
+
+    # Creating a new DataFrame 'encoded_df' with the encoded data and column names
+    encoded_df_cat = pd.DataFrame(encoded_data_cat, columns=encoder.get_feature_names_out(categorical_columns))
+    encoded_df_cat.head()
+
+    return encoded_data_cat
+
+def df_to_normalized_ndarray(df_num, df):
+    # 3--> Normalizing numerical columns
+
+    # Creating an instance of the MinMaxScaler for normalization
+    scaler = MinMaxScaler()
+
+    # Creating an empty DataFrame to store the normalized numerical columns
+    normalized_numerical = pd.DataFrame()
+
+    # Iterating over each numerical column
+    for column in df_num:
+        try:
+            # Normalizing the column values using the scaler and flattening the result
+            normalized_column = scaler.fit_transform(df[[column]])
+            normalized_numerical[column] = normalized_column.flatten()
+        except ValueError:
+            # Skipping the column if it contains non-numeric values
+            print(f"Skipping column {column} due to non-numeric values.")
+
+    # Displaying the head of the normalized_numerical DataFrame
+    normalized_numerical.head()
+
+    # 4--> Converting remaining data to a numerical array
+
+    # Converting the normalized_numerical DataFrame to a numerical array
+    data_array = np.array(normalized_numerical)
+
+    # Printing the shape (dimensions) of the data array
+    print(data_array.shape)
+
+    # Accessing and printing the first row of the data array
+    print(data_array[0])
+
+    return data_array
+
+def preprocessing():
+    dataset_paths = ["Data/07_17_2023_v1_GeneID_Query__SSTR1.csv"]
+    preprocessed_data = []
+
+    for path in dataset_paths:
+        result_data = []
+
+        # 2--> Loading data and preparing the dataset
+        # Loading the dataset from a CSV file into a pandas DataFrame
+        df = pd.read_csv(path)
+        df.drop_duplicates(inplace=True)
+        df = df.set_index("REM_ID")
+
+        with open('CCA_preprocessing.out', 'w') as saveFile:
+            saveFile.write("Successful Run")
+            saveFile.write("\n")
+            saveFile.write(str(df.columns.values))
+            saveFile.write(str(df))
+
+        y = df['Predicted_function']  # label vector
+        X = df.drop(columns=['Predicted_function'], axis=1)  # Dataset without labels
+        y_cat = df['Predicted_function']
+        # Creating a DataFrame 'df_cat' containing only the categorical attributes
+        df_cat = X.select_dtypes(exclude=["number"])  # Categorical attributes only
+
+        # Creating a list 'df_num' of column names for the numeric attributes
+        df_num = X.select_dtypes(include=["number"]).columns.tolist()  # Numeric attributes only
+
+        # Print all Attack Categories
+        all_cat = []
+        for i in y_cat:
+            if i not in all_cat:
+                # print(i)
+                all_cat.append(i)
+
+        data_array = df_to_normalized_ndarray(df_num, df)
+        encoded_data_cat = categorical_data_to_numerical_encoding(df_cat)
+        print("Shape of df_cat : ", df_cat.shape)
+        print("first element of df_cat :", df_cat.head)
+        print("Shape of encoded_data_cat : ", encoded_data_cat.shape)
+        print("first element of encoded_data_cat :", encoded_data_cat[0])
+
+        # 6--> Concatenating encoded categorical data with numerical data array
+
+        # Concatenate encoded categorical data with numerical data array
+        concatenated_data = np.concatenate((encoded_data_cat, data_array), axis=1)
+        # Print the shape of the concatenated data
+        print("Shape of concatenated data:", concatenated_data.shape)
+
+        # Print the first row of the concatenated data
+        print("First row of concatenated data:", concatenated_data[0])
+
+        result_data.append(concatenated_data)
+        print("Shape of concatenated_data : ", concatenated_data.shape)
+        result_data.append(y_cat)
+        print("Shape of y_cat : ", y_cat.shape)
+
+        result_data.append(all_cat)
+        print("Shape of all_cat : ", all_cat[0])
+
+        print(len(result_data))
+        preprocessed_data.append(result_data)
+
+    with open("preprocessedData.txt", "w") as output:
+        for row in preprocessed_data:
+            s = " ".join(map(str, row))
+            output.write(s + '\n')
+
+        
+    return preprocessed_data
+
+
+if __name__ == '__main__':
+    data = preprocessing()
+    for dataset_idx, dataset in enumerate(data):
+        concatenated_data, y_cat, all_cat = dataset
+        print(f"Dataset {dataset_idx + 1} - Number of Samples:", len(concatenated_data))
+        print(f"Dataset {dataset_idx + 1} - Number of Labels:", len(y_cat))
+        print("===")
+    
